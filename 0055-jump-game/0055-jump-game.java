a05b78212f54805d7549ad3b2d26d64e5ae7cc5b
KhushiBhardwaj11 <140068608+KhushiBhardwaj11@users.noreply.github.com>
2024-04-01 04:24:38 -0700

    Time: 389 ms (7.43%), Space: 45.9 MB (14.22%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -4,18 +4,16 @@ class Solution {
         int maxReach = 0;
         for (int i = 0; i < n; i++) {
             if (maxReach < i) {
-                // If the current index is beyond the maximum reachable index,
-                // we can't proceed further.
                 return false;
             }
             System.out.println("i: " + i + ", maxReach: " + maxReach);
             maxReach = Math.max(maxReach, i + nums[i]);
             if (maxReach >= n - 1) {
-                // If we can reach or go beyond the last index, return true.
+              
                 return true;
             }
         }
-        return true; // We can reach the last index.
+        return true; 
     }
     
     public static void main(String[] args) {
